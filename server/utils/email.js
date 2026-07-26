1530b00aa42cab2263e27b087d18f066c19a0854
Krishnandan2005 <krishnandannitb@gmail.com>
2026-07-26 19:53:05 +0530

    Fix email IPv6 connection issue on Render

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,7 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    family: 4, // force IPv4 to avoid Render's IPv6 ENETUNREACH issue
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
